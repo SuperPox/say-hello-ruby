2f5a58d9889eb8c1036c23748febc279b97604b2
SuperPox <calebkohl@gmail.com>
2021-01-12 23:45:47 -0700

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,4 @@
 # Build your say_hello method here
-def say_hello(name="Your Conscious", description="unnoticed")
-    puts "I am your #{name}, I feel #{description}"
+def say_hello(name="Ruby Programmer")
+    puts "Hello #{name}!"
 end
